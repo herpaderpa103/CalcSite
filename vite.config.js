5721d534098dbba5eb1557a51bcb50fece09987a
herpaderpa103 <herpaderpa103@gmail.com>
2025-04-11 01:19:33 -0400

    new config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CalcSite/', // Replace <your-repo-name> with your GitHub repository name
+  base: '/CalcSite', // Replace <your-repo-name> with your GitHub repository name
 })
