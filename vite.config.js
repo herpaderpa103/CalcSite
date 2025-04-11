6acb9654bf7df21573e99cf7ddb9cfd47c80aedd
herpaderpa103 <herpaderpa103@gmail.com>
2025-04-11 01:08:32 -0400

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
-    base: '/CalcSite/',
-  }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // Or your framework plugin
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/CalcSite/', // Replace <your-repo-name> with your GitHub repository name
+})
